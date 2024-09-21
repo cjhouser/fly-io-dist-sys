f43b9544214020494698071414128d69ff8cdc79
Charles Houser <cjohouser@gmail.com>
2024-09-21 23:14:24 +0000

    single node uuid

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 
+	// Echo
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
@@ -21,6 +22,22 @@ func main() {
 		body["type"] = "echo_ok"
 
 		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	// Unique ID Generation
+	current_uid := 0
+	n.Handle("generate", func(msg maelstrom.Message) error {
+
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = current_uid
+		current_uid++
+
 		return n.Reply(msg, body)
 	})
 
